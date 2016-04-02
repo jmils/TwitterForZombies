dae34a2fee5416d81431517e353796c19c0592ee
jmils <jamie.milanese@gmail.com>
2016-04-02 17:54:23 -0400

    add assignment and role models, migrate

--- TwitterForZombies/app/models/zombie.rb
+++ TwitterForZombies/app/models/zombie.rb
@@ -1,9 +1,13 @@
 class Zombie < ActiveRecord::Base
+  has_one :brain, dependent: :destroy
+  has_many :assignments
+  has_many :roles, through: :assignments
+
   scope :rotting, where(rotting:  true)
   scope :fresh, where("age < 20")
   scope :recent, order("created_at desc").limit(3)
 
-  has_one :brain, dependent: :destroy
+
   before_save :make_rotting
 
   def make_rotting
